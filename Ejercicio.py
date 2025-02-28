f39c1cfb870e552270ae4bbcd725b50fea40c901
m4riod14z <mariomd2807@gmail.com>
2025-02-27 19:35:17 -0500

    Add files via upload

--- Ejercicio.py
+++ Ejercicio.py
@@ -4,12 +4,13 @@ from functools import reduce
 n = int(input("Ingrese la cantidad de terminos que desea: "))
 
 nums = list(range(0, n+1))
+x = int(input("Ingrese el valor de X que desea: "))
 
 def fun_map(n):
-    return 1 / factorial(n)
+    return (x**n) / factorial(n)
 
 def fun_redux(a,b):
     return a+b
 
 aprox = reduce(fun_redux, list(map(fun_map, nums)))
-print(f"Aproximacion de e: {aprox} ")
+print(f"Aproximacion de e^{x}: {aprox}")
